0a10ff8554e6ead52d5b22424a3b75cbd51184d1
Jordan Haskovec <jorhaskovec@gmail.com>
2015-04-11 14:02:46 -0500

    working on implementing receiving in ncurses

--- NcursesGet.py
+++ NcursesGet.py
@@ -1,6 +1,9 @@
 import curses
 from curses import wrapper
 
+def ClearByte():
+        return [-1,-1,-1,-1,-1,-1,-1,-1]
+
 def PrintTitle(stdscr,Xmax):
     Title = "Ultras0nic Air-Gap Bridger"
     Xloc = (Xmax/2) - (len(Title)/2)
@@ -16,12 +19,29 @@ def main(stdscr):
     ScreenSize = stdscr.getmaxyx()
     Ymax = ScreenSize[0]
     Xmax = ScreenSize[1]
-
     stdscr.border()
     PrintTitle(stdscr,Xmax)
+
+    # Starting Freq Analysis Stuff
+    p = subprocess.Popen('chuck chuck_files/FFT.ck 2>&1 > /dev/null', 
+            shell=True, stdout = subprocess.PIPE,)
+    CurrentFreq = 0
+    FreqList = [1,2,3,4]
+    CurrentByte = [-1,-1,-1,-1,-1,-1,-1,-1]
+    ByteString = ""
+    JustCaught = 0
+    streak = False
+    PrintBit = False
+    StartBit = False
+
     while True:
         stdscr.refresh()
         #Do things
         
+        while p.poll() is None:
+            output = p.stdout.readline()
+            if "Spacer" in output:
+                #print ''
+                pass
 
 wrapper(main)
